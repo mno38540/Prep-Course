cb8bc45133fc5c0ffeaf782c56a6f1d689baf77e
Andres <medinac228@gmail.com>
2022-02-14 12:20:38 -0500

    Se finaliza el homework clase JSI Test Suites: 1 passed, 1 total   Tests:       35 passed, 35 total , y se trabaja en el trabajo de la clase JSII se deja a mitad con un totla de 20 por pasar

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -80,11 +80,11 @@ function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (str1.lenght== str2.lenght){
-    return true;
+  if (str1.length !=  str2.length){
+    return false;
   }
   else{
-    return false;
+    return true;
   }
 }
   
@@ -117,7 +117,7 @@ function mayorQueCincuenta(num) {
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  return x/y;
+  return x%y;
 }
 
 function esPar(num) {
@@ -266,6 +266,12 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
+  if (typeof letra === 'string' && letra.length === 1) {
+    return letra.match(/[aeiou]/) ? 'Es vocal' : 'Dato incorrecto'
+    } else {
+    return 'Dato incorrecto';
+  }
+
   
 }
 
